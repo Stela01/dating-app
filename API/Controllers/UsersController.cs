9a0fcc3de8e26615678d9401bab78cd50b7169c2
Stela01 <stela.ilic01@gmail.com>
2026-03-12 16:44:14 +0100

    Add feature components and user authentication state management

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ namespace API.Controllers
    [Authorize]
    public class UsersController(DataContext context) : BaseApiController
    {
+   [AllowAnonymous] //dodala da bi se prikazali useri
    [HttpGet]
    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
    {

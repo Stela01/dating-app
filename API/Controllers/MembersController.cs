f3d7b481183e7ec5f41022aaa1be9a2bb55b93b1
Stela01 <stela.ilic01@gmail.com>
2026-06-18 17:37:02 +0200

    Add member repository and refactor controller

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,32 @@
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Authorize]
+public class MembersController(IMemberRepository memberRepository) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
+    {
+        return Ok(await memberRepository.GetMembersAsync());
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Member>> GetMember(int id)
+    {
+        var member = await memberRepository.GetMemberByIdAsync(id);
+
+        if (member == null) return NotFound();
+
+        return member;
+    }
+
+    [HttpGet("{id:int}/photos")]
+    public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(int id)
+    {
+        return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+    }
+}
